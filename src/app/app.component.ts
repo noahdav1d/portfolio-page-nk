3804f991243064a0fe6082786e7a446ca5eec45a
Noah Kiefer <noah.kiefer@students.bfh.ch>
2025-03-13 15:31:45 +0100

    create components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { CvComponent } from './cv/cv.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // AppComponent ist jetzt eine Standalone-Komponente
+  imports: [
+    HeaderComponent,
+    HeroComponent,
+    AboutMeComponent,
+    CvComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'portfolio-page-nk';
